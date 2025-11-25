5ec49f75700a61bbc2ec7a8663c8b989caf6ab02
Alexia953-hub <alexiaadams953@gmail.com>
2025-11-25 10:03:11 -0700

    working on for loop in yahtzee

--- Cup.java
+++ Cup.java
@@ -5,11 +5,8 @@ public class Cup
         private ArrayList<Integer> playerCup = new ArrayList<Integer>();
         private ArrayList<Integer> heldDice = new ArrayList<Integer>();
         private int value = 0;
+        // use one single cup for all players?
         
-        public void makeCup(int playerNum)
-        {
-            
-        }
         public int roll()
         {
             // rolls the dice
@@ -25,6 +22,8 @@ public class Cup
             heldDice.add(diceToHold);
 
         }
+        
+        
         public void OtherDiceRoll()
         {
             // rerolls the non held dice
@@ -33,5 +32,27 @@ public class Cup
              roll();
              playerCup.add(this.value);
             }
+        }
+        public void rollAllDice()
+        {
+            playerCup.clear();
+            for(int i = 0; i < 6; i++)  
+                {
+                    roll();
+                    playerCup.add(this.value);
+                }    
+        }
+        
+        
+        
+        
+        public ArrayList getHeldDice()
+        {
+            System.out.println(heldDice);
+            return heldDice;
+        }
+        public ArrayList getPlayerCup()
+        {
+            return playerCup;
         }
     }
--- Yahtzee.java
+++ Yahtzee.java
@@ -6,6 +6,7 @@ public class Yahtzee
     
     public static void main (String[] args)
     {
+        Cup newCup = new Cup();
         Scanner playerNumber = new Scanner(System.in);
         System.out.println("How many players? ");           // figuring out the number of players
         int playerNum = playerNumber.nextInt();
@@ -21,8 +22,30 @@ public class Yahtzee
             Players player = new Players(playerName);          // create new player object for each player in the list           
             playerList.add(player);
         }
+        for(Players players : playerList)                      // for player in playerlist
+        {
+            int turns = 0;
+            newCup.rollAllDice();
+            ArrayList nonHeldDice = newCup.getPlayerCup();
+            while(nonHeldDice.size() > 0 || turns == 0)
+            {
 
-        
+                for(int i = 0; i <= nonHeldDice.size(); i++ )
+                {
+                    int diceNum = i + 1;
+                    System.out.println(nonHeldDice.get(i) + " is the dice number " + diceNum);
+                }
+                                                                    // asking if want to keep any dice
+                Scanner ScannerDiceHolder = new Scanner(System.in);
+                System.out.println("How many dice would you like to keep? ");
+                int heldDice = ScannerDiceHolder.nextInt();
+                for(int x = 0; x <= heldDice)
+
+            }
+        }
+
+    
+
         // Score scoreObject = new Score();
         // scoreObject.printScoreSheet();
 
